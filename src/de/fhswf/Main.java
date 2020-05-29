a54491182669895f97aa10fef398d73df4df1f50
xonex2020 <dominik.fh@web.de>
2020-05-29 13:22:27 +0200

    TEST

--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -3,7 +3,7 @@ package de.fhswf;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Dies ist ein Test.");
+		System.out.println("Dies ist eQDDQWDQDQDQWD Test.");
 	}
 	
 }
