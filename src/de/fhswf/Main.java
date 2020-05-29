c69525fb8f13818abc17c9185249e45c80cc85ce
Pascal <Pascal@DESKTOP-ULQJTNC>
2020-05-29 13:36:32 +0200

    Test Pascal

--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -3,7 +3,7 @@ package de.fhswf;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Dies ist eQDDQWDQDQDQWD Test.");
+		System.out.println("Augen auf beim Eierkauf.");
 	}
 	
 }
