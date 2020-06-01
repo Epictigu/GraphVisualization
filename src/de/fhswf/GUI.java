f4c3322533504606ba2fbf237d9d93ec013043ec
Gin-Wah <Gin-Wah@DESKTOP-HRSRKV0>
2020-06-01 10:36:28 +0200

    JFrame

---
+++ src/de/fhswf/GUI.java
@@ -0,0 +1,30 @@
+package de.fhswf;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    public GUI() {
+	initWindow();
+    }
+
+    private void initWindow() {
+	setTitle("GDI Projekt");
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setSize(500, 650);
+	this.setResizable(false);
+	setLocationRelativeTo(null); // setzt das Fenster in die Mitte des Bildschirms
+	setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+
+    }
+}
--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -1,17 +1,14 @@
 package de.fhswf;
 
 public class Main {
-	
-	/*TODO
-	 * FileSystem
-	 * JFrame anlegen
-	 * Pixelberechnung
-	 * Zugriff auf Dateiexplorer
-	 * Zeichnen von Knoten/Kanten
-	 */
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World.");
-	}
+
+    /*
+     * TODO FileSystem JFrame anlegen Pixelberechnung Zugriff auf Dateiexplorer
+     * Zeichnen von Knoten/Kanten
+     */
+
+    public static void main(String[] args) {
+	new GUI();
+    }
 
 }
