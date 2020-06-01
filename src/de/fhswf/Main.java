e7d97afa01761f4c64136816e782ea313600ea0c
Gin-Wah <Gin-Wah@DESKTOP-HRSRKV0>
2020-06-01 10:15:44 +0200

    Test Gin

--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -1,9 +1,12 @@
 package de.fhswf;
 
-public class Main {
-	
-	public static void main(String[] args) {
+public class Main
+{
+
+	public static void main(String[] args)
+	{
 		System.out.println("Augen auf beim Eierkauf.");
+		System.out.println("AAAAAHHHHHHH");
 	}
-	
+
 }
