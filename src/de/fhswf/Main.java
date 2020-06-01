f622002e5b86f715de33cc5919f2a1ce3e5b832e
Gin-Wah <Gin-Wah@DESKTOP-HRSRKV0>
2020-06-01 11:10:50 +0200

    TODO

--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -2,12 +2,14 @@ package de.fhswf;
 
 public class Main {
 
-//	  TODO
-//	  FileSystem
-//	  JFrame anlegen
-//	  Pixelberechnung
-//	  Zugriff auf Dateiexplorer
-//	  Zeichnen von Knoten/Kanten
+//	  TODO:
+//	  FileSystem					Gin-Wah
+//			Dekodieren
+//	  JFrame anlegen				Gin-Wah
+//			UI
+//	  Zugriff auf Dateiexplorer		Pascal
+//	  Zeichnen von Knoten/Kanten	Timo, Dominik
+//	  		Pixelberechnung			Timo, Dominik
 
 	public static void main(String[] args) {
 		GUI gui = new GUI();
