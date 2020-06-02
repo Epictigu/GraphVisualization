a1799416891bf18dfe0ebe34c039aacc0d5fe5c6
Gin-Wah <Gin-Wah@DESKTOP-HRSRKV0>
2020-06-02 11:42:38 +0200

    Graph Klasse

---
+++ src/de/fhswf/Graph.java
@@ -0,0 +1,45 @@
+package de.fhswf;
+
+public class Graph
+{
+	private int amountKnots;
+	private String[] knotNames;
+	private boolean[][] adjacencyMatrix;
+
+	public Graph()
+	{
+	}
+
+	public void init(int amountKnots)
+	{
+		this.amountKnots = amountKnots;
+		knotNames = new String[amountKnots + 1];
+		adjacencyMatrix = new boolean[amountKnots + 1][amountKnots + 1];
+	}
+
+	public void writeToKnotNames(int index, String name)
+	{
+		knotNames[index] = name;
+	}
+
+	public void writeLineToAdjacencyMatrix(int x, int y)
+	{
+		adjacencyMatrix[x][y] = true;
+	}
+
+	public int getAmountKnots()
+	{
+		return amountKnots;
+	}
+
+	public String[] getKnotNames()
+	{
+		return knotNames;
+	}
+
+	public boolean[][] getAdjacencyMatrix()
+	{
+		return adjacencyMatrix;
+	}
+
+}
