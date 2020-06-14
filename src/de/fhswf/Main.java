e0e0a0c9354cce75fe6215dc6c2c9b9934a5f89e
Tigusa <tigus@Tigusa.fritz.box>
2020-06-14 10:42:49 +0200

    Removed Internal TODO

--- src/de/fhswf/Main.java
+++ src/de/fhswf/Main.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-//	  TODO:
-//	  FileSystem					Gin-Wah, Pascal
-//			Dekodieren				Gin-Wah, Pascal
-//	  JFrame anlegen				Gin-Wah
-//			UI
-//	  Zugriff auf Dateiexplorer		Pascal
-//	  Zeichnen von Knoten/Kanten	Timo, Dominik
-//	  		Pixelberechnung			Timo, Dominik
 	
 	public static List<GUI> guiList = new ArrayList<GUI>();
 	
